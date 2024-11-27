55476c0ce18aeec303470499fc2a302d3ca2bba7
normaIyad <normamohsan39@gmail.com>
2024-11-27 16:53:02 +0200

    first commit

---
+++ class_app/Prodact.cs
@@ -0,0 +1,14 @@
+﻿namespace class_app
+{
+    internal class Prodact
+    {
+        public int id;
+        public string name;
+        public string description;
+        public float price;
+        public void print()
+        {
+            Console.WriteLine($"prodact name : {name} prodact description : {description} price : {price}");
+        }
+    }
+}
---
+++ class_app/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace class_app
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Prodact prodact1 = new Prodact();
+            prodact1.name = "car";
+            prodact1.price = 10000;
+            prodact1.id = 100;
+            prodact1.description = "Description pla pla pla pla";
+            prodact1.print();
+        }
+    }
+}
